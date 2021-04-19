1ea205b702b926068364e85179dd75194fedb685
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-19 14:47:16 +0600

    5. AppBar

--- apple/lib/ui/garden.dart
+++ apple/lib/ui/garden.dart
@@ -1,3 +1,4 @@
+import 'package:apple/ui/constants.dart';
 import 'package:flutter/material.dart';
 
 class GardenPage extends StatefulWidget {
@@ -8,6 +9,32 @@ class GardenPage extends StatefulWidget {
 class _GardenPageState extends State<GardenPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: cTextColor,
+            ),
+            onPressed: () {}),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.search_rounded,
+                color: cTextColor,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.shopping_cart_rounded,
+                color: cTextColor,
+              ),
+              onPressed: () {}),
+          SizedBox(width: cPadding / 2),
+        ],
+      ),
+    );
   }
 }
