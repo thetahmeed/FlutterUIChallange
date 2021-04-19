9d4b772945f0072159d9d8810405d59e85568120
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-19 14:35:46 +0600

    3.1 Dummy Data

--- apple/lib/models/Product.dart
+++ apple/lib/models/Product.dart
@@ -14,3 +14,54 @@ class Product {
       this.id,
       this.color});
 }
+
+List<Product> products = [
+  Product(
+    id: 0,
+    image: 'assets/images/watch.png',
+    price: 1200,
+    size: 10,
+    description: descriptionText,
+    color: Color(0xFF3D82AE),
+    name: 'Awesome Bag',
+  ),
+  Product(
+    id: 1,
+    image: 'assets/images/watch.png',
+    price: 1200,
+    size: 10,
+    description: descriptionText,
+    color: Color(0xFF3D82AE),
+    name: 'Awesome Bag',
+  ),
+  Product(
+    id: 2,
+    image: 'assets/images/watch.png',
+    price: 1200,
+    size: 10,
+    description: descriptionText,
+    color: Color(0xFF3D82AE),
+    name: 'Awesome Bag',
+  ),
+  Product(
+    id: 3,
+    image: 'assets/images/watch.png',
+    price: 1200,
+    size: 10,
+    description: descriptionText,
+    color: Color(0xFF3D82AE),
+    name: 'Awesome Bag',
+  ),
+  Product(
+    id: 4,
+    image: 'assets/images/watch.png',
+    price: 1200,
+    size: 10,
+    description: descriptionText,
+    color: Color(0xFF3D82AE),
+    name: 'Awesome Bag',
+  ),
+];
+
+String descriptionText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis metus vitae odio elementum, id commodo nisl varius. Duis sed pharetra nisl, et pharetra mi. Pellentesque nibh nisl, rutrum a justo posuere, pellentesque egestas velit.';
