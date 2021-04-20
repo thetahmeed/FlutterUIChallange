836ba7f7ba28b54bb92ba914ae89db348b8a3476
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-20 16:16:39 +0600

    Details screen

--- apple/lib/details/details_screen.dart
+++ apple/lib/details/details_screen.dart
@@ -1,4 +1,6 @@
+import 'package:apple/details/comaponents/body.dart';
 import 'package:apple/models/Product.dart';
+import 'package:apple/ui/constants.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
@@ -9,7 +11,21 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: product.color,
+      appBar: mAppBar(),
+      body: Body(product: product),
+    );
+  }
+
+  AppBar mAppBar() {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      actions: [
+        IconButton(icon: Icon(Icons.search_rounded), onPressed: () {}),
+        IconButton(icon: Icon(Icons.shopping_cart_rounded), onPressed: () {}),
+        SizedBox(width: cPadding / 2),
+      ],
     );
   }
 }
