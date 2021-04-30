e322eb653bcccaa86a28e723a8168d41906c086b
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-30 12:38:39 +0600

    Main Screen

---
+++ apple/lib/inkfiles/components/home/body.dart
@@ -0,0 +1,14 @@
+import 'package:apple/inkfiles/components/home/search_box.dart';
+import 'package:apple/inkfiles/constants.dart';
+import 'package:flutter/material.dart';
+
+class InkBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        mSearchBox(),
+      ],
+    );
+  }
+}
