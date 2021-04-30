f6188c6531dcfa5c543ad5ebdcd962a1449ee386
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-30 16:15:05 +0600

    Single Product demo

---
+++ apple/lib/inkfiles/components/home/single_product.dart
@@ -0,0 +1,104 @@
+import 'package:apple/inkfiles/constants.dart';
+import 'package:apple/inkfiles/product.dart';
+import 'package:flutter/material.dart';
+
+class singleProduct extends StatelessWidget {
+  const singleProduct({
+    Key key,
+    this.index,
+    this.product,
+    this.press,
+  }) : super(key: key);
+
+  final int index;
+  final Product product;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: cDefaultPadding,
+        vertical: cDefaultPadding / 2,
+      ),
+      height: 160,
+      child: InkWell(
+        onTap: press,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              height: 136,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(22),
+                color: index.isEven ? cBlueColor : cSecondaryColor,
+                boxShadow: [cDefaultShadow],
+              ),
+              child: Container(
+                margin: EdgeInsets.only(right: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: cDefaultPadding),
+                height: 160,
+                width: 200,
+                child: Image.asset(
+                  product.image,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              bottom: 0,
+              child: SizedBox(
+                height: 136,
+                width: size.width - 200,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: cDefaultPadding),
+                      child: Text(
+                        product.title,
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: cDefaultPadding * 1.5,
+                          vertical: cDefaultPadding / 4),
+                      decoration: BoxDecoration(
+                        color: cSecondaryColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(22),
+                          topRight: Radius.circular(22),
+                        ),
+                      ),
+                      child: Text(
+                        '\$${product.price}',
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
