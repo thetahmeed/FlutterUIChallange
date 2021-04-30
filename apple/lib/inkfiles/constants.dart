f100bf7fe071de8b191d21e75ee5b76d4e0180e3
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-30 12:37:22 +0600

    Constants values

---
+++ apple/lib/inkfiles/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// list of colors that we use in our app
+const cBackgroundColor = Color(0xFFF1EFF1);
+const cPrimaryColor = Color(0xFF035AA6);
+const cSecondaryColor = Color(0xFFFFA41B);
+const cTextColor = Color(0xFF000839);
+const cTextLightColor = Color(0xFF747474);
+const cBlueColor = Color(0xFF40BAD5);
+
+const cDefaultPadding = 20.0;
+
+// our default Shadow
+const cDefaultShadow = BoxShadow(
+  offset: Offset(0, 15),
+  blurRadius: 27,
+  color: Colors.black12, // Black color with 12% opacity
+);
