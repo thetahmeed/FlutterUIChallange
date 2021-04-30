1e9ad66fd72c764945e81d29f34f4e067a7e9467
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-30 16:14:53 +0600

    Added safe area

--- apple/lib/inkfiles/components/home/body.dart
+++ apple/lib/inkfiles/components/home/body.dart
@@ -1,14 +1,47 @@
+import 'package:apple/inkfiles/components/home/categories.dart';
 import 'package:apple/inkfiles/components/home/search_box.dart';
+import 'package:apple/inkfiles/components/home/single_product.dart';
 import 'package:apple/inkfiles/constants.dart';
+import 'package:apple/inkfiles/product.dart';
 import 'package:flutter/material.dart';
 
 class InkBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        mSearchBox(),
-      ],
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          mSearchBox(searchedValue: (value) {}),
+          mCategory(),
+          SizedBox(height: cDefaultPadding / 1),
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 70),
+                  decoration: BoxDecoration(
+                    color: cBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  itemCount: inkProductList.length,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) => singleProduct(
+                    index: index,
+                    product: inkProductList[index],
+                    press: () {},
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
