80ec7ff4d506deb361b3753bcb10ba39d2be8bd2
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-05-01 13:37:57 +0600

    Product image

---
+++ apple/lib/inkfiles/components/details/product_image.dart
@@ -0,0 +1,41 @@
+import 'package:apple/inkfiles/constants.dart';
+import 'package:flutter/material.dart';
+
+class productImage extends StatelessWidget {
+  const productImage({
+    Key key,
+    @required this.size,
+    this.image,
+  }) : super(key: key);
+
+  final Size size;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: cDefaultPadding),
+      width: double.infinity,
+      height: size.width * 0.8,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            height: size.width * 0.7,
+            width: size.width * 0.7,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+          ),
+          Image.asset(
+            image,
+            height: size.width * 0.75,
+            width: size.width * 0.75,
+            fit: BoxFit.contain,
+          ),
+        ],
+      ),
+    );
+  }
+}
