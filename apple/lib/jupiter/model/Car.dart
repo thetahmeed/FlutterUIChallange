f8d42448f108aed4636e70f4aa926db7e8bbb98d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-07-03 14:50:05 +0600

    Model class

---
+++ apple/lib/jupiter/model/Car.dart
@@ -0,0 +1,26 @@
+class Car {
+  final int position;
+  final String img;
+  final String title;
+  final String description;
+  final String phone;
+  final String sms;
+
+  Car(
+    this.position, {
+    this.img,
+    this.title,
+    this.description,
+    this.phone,
+    this.sms,
+  });
+}
+    title: 'Hiace',
+    description: 'Looking for a 10 seater?',
+    phone: '01700547406',
+    sms: 'm.me/tahmeedslab',
+  ),
+  Car(
+    4,
+    img:
+        'https://png2.cleanpng.com/sh/cc95d408c2ae739c47c8e3f8f9599658/L0KzQYm3VME5N6ZniZH0aYP2gLBuTgRwgZD5eZ9xaXHmdX7qggIufpJzReZ4eXB3cX7qgf1zgV55hAt4dHGwRbLrVvRmOJI8fNU5ZEexRoK7WcQ5P2Q2TaQ9MEOzSYm9VcE2OV91htk=/kisspng-toyota-hiace-car-van-toyota-camry-toyota-5ad6de0a7dc0d7.6149487315240309865151.png',
