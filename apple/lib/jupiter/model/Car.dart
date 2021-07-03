27ba1a1941107f4c1699b85766096f6fba5cf141
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-07-03 14:50:50 +0600

    Sample data

--- apple/lib/jupiter/model/Car.dart
+++ apple/lib/jupiter/model/Car.dart
@@ -15,6 +15,28 @@ class Car {
     this.sms,
   });
 }
+
+List<Car> carList = [
+  Car(
+    1,
+    img: 'https://pngimg.com/uploads/lexus/lexus_PNG58.png',
+    title: 'Sedan',
+    description: 'Looking for a 4 seater?',
+    phone: '01700547406',
+    sms: 'm.me/tahmeedslab',
+  ),
+  Car(
+    2,
+    img: 'https://pngimg.com/uploads/toyota/toyota_PNG1952.png',
+    title: 'Noah',
+    description: 'Looking for a 7 seater?',
+    phone: '01700547406',
+    sms: 'm.me/tahmeedslab',
+  ),
+  Car(
+    3,
+    img:
+        'https://png2.cleanpng.com/sh/dc74a4f0b7b2df49804a4fee86ff8dab/L0KzQYm3U8MxN6VrfZH0aYP2gLBuTgRwgZD5eZ9xaXz4iH7qggIufJDAhAZqLXzkfrW0gCJ2caRqip95cnHnf377jClwfJIyjtN3LUXkcrXqhcczOJU7faY6LkC2Q4aBVMQ0OWY3SqUDOEW8RIG8Vb5xdpg=/kisspng-toyota-hilux-car-toyota-land-cruiser-prado-toyota-van-5abdce720d6e41.033584431522388594055.png',
     title: 'Hiace',
     description: 'Looking for a 10 seater?',
     phone: '01700547406',
@@ -24,3 +46,9 @@ class Car {
     4,
     img:
         'https://png2.cleanpng.com/sh/cc95d408c2ae739c47c8e3f8f9599658/L0KzQYm3VME5N6ZniZH0aYP2gLBuTgRwgZD5eZ9xaXHmdX7qggIufpJzReZ4eXB3cX7qgf1zgV55hAt4dHGwRbLrVvRmOJI8fNU5ZEexRoK7WcQ5P2Q2TaQ9MEOzSYm9VcE2OV91htk=/kisspng-toyota-hiace-car-van-toyota-camry-toyota-5ad6de0a7dc0d7.6149487315240309865151.png',
+    title: 'Hice Grand Cabin',
+    description: 'Looking for a 14 seater?',
+    phone: '01700547406',
+    sms: 'm.me/tahmeedslab',
+  ),
+];
